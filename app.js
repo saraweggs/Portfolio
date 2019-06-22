e77151879fb601535d5d1c08ba92366215d6979e
Sara Wegmann <sarawegmann@Saras-MacBook-Air.local>
2019-06-22 19:15:26 -0400

    almost completed portfolio site

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 // Sticky Header
 $(window).scroll(function() {
 
@@ -8,22 +9,8 @@ $(window).scroll(function() {
     }
 });
 
-// Mobile Navigation
-$('.mobile-toggle').click(function() {
-    if ($('.main_h').hasClass('open-nav')) {
-        $('.main_h').removeClass('open-nav');
-    } else {
-        $('.main_h').addClass('open-nav');
-    }
-});
 
-$('.main_h li a').click(function() {
-    if ($('.main_h').hasClass('open-nav')) {
-        $('.navigation').removeClass('open-nav');
-        $('.main_h').removeClass('open-nav');
-    }
-});
-
+// Navigation scroll
 $('nav a').click(function(event) {
     var id = $(this).attr("href");
     var offset = 70;
